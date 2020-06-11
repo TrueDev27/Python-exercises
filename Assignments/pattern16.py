47ce4610e1aa70981898d6a8560fec4caefc2899
TrueDev27 <TrueDev27@gmail.com>
2020-06-11 14:58:01 +0530

    Update pattern16.py

--- Assignments/pattern16.py
+++ Assignments/pattern16.py
@@ -1,15 +1,33 @@
 """
     Program to display a Pascal's Triange
-
     input should be number of rows.
+    input  = 4 
+            1
+        1       1
+    1       2       1
+1       3       3       1
+
 """
-n = int(input("Enter a number: \n"))
-#n = 4
-for i in range(1, n + 1):           # row
-    for j in range(n - i, 0, -1):   # col
-        print(end=" \t")            # printing  blank + tab 
-    for j in range(1, i + 1):       # col 
-      print(i + j - 1, end="\t")    # prining incremental
-    for j in range(i - 1, 0 , -1):  # col
-      print(j + i - 1, end="\t")    # printing decremental
-    print()
+#n = int(input("Enter a number: \n"))
+n = 4
+#a = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]
+a = []
+for i in range(1, n + 1) :
+    a.append([1])
+for i in range(len(a)) :
+   if i == len(a) :
+    a[i].append(1)
+   else :
+    a[i].append(i+1)
+
+
+    
+print(a)
+
+     
+""" for i in range(n):                      # row
+    for j in range(n - i, 0, -1):       # col
+        print(end=" \t")                # printitng blank + tabs    
+    for j in range(i+1):                  # col 
+        print(a[i][j] , end="\t\t")
+    print() """
